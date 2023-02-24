26bbae79d72bf9352a4e5fd197dd811e4f2d5337
Max Tobias Weber <tobias.weber@wheregroup.com>
2023-02-24 11:14:57 +0100

    fix responsive layout

--- webgis-demo/src/ra_components/GisLayout.jsx
+++ webgis-demo/src/ra_components/GisLayout.jsx
@@ -38,9 +38,7 @@ const Content = styled("div")(({ theme }) => ({
   display: "flex",
   flexDirection: "row",
   justifyContent: "center",
-  flexGrow: 2,
-  padding: "0 3em",
-  maxHeight: "800px",
+  maxHeight: "50vh",
   position: "fixed",
   bottom: "0",
   width: "100%",
@@ -50,22 +48,31 @@ const Content = styled("div")(({ theme }) => ({
   boxShadow: "0px 0px 8px rgba(0,0,0,0.2)",
 }));
 
+const ContentWrapper = styled(Box)(({ theme }) => ({
+  maxWidth: "600px",
+  width: "100%",
+}));
+
 const GisLayout = ({ children, dashboard, title }) => {
   const [open] = useSidebarState();
   const [contentOpen, setContentOpen] = useState(true);
 
   React.useEffect(() => {
-    setContentOpen(true)
-  },[children])
+    setContentOpen(true);
+  }, [children]);
   return (
     <Root>
       <AppFrame>
-        <AppBar title={title} open={open} sx={{pointerEvents:'all'}} />
+        <AppBar title={title} open={open} sx={{ pointerEvents: "all" }} />
         <ContentWithSidebar>
           <Sidebar sx={{ backgroundColor: "#f0f0f0", pointerEvents: "all" }}>
             <Menu hasDashboard={!!dashboard} />
           </Sidebar>
-          <Content sx={{...(contentOpen?{}:{height:'40px', overflow:'hidden'})}}>
+          <Content
+            sx={{
+              ...(contentOpen ? {} : { height: "40px", overflow: "hidden" }),
+            }}
+          >
             <IconButton
               onClick={() => setContentOpen((val) => !val)}
               sx={{ position: "absolute", top: 0, zIndex: 100 }}
@@ -76,7 +83,7 @@ const GisLayout = ({ children, dashboard, title }) => {
                 <KeyboardArrowUpIcon />
               )}
             </IconButton>
-            <Box sx={{ maxWidth: "600px", width: "100%" }}>{children}</Box>
+            <ContentWrapper>{children}</ContentWrapper>
           </Content>
         </ContentWithSidebar>
       </AppFrame>
